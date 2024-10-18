1b542e05e712a894cb8a90588ad48b8c74807662
david de la puente enriquez <Twinkymen@hotmail.com>
2024-10-18 16:07:47 +0200

    Saludo class Updated

--- src/saludo.php
+++ src/saludo.php
@@ -3,16 +3,19 @@ namespace Saludo\Saludo;
 
 Class saludo{
 private $msn;
+private $usuario;
 
-public function __construct(){
-$this->setSaludo();
+public function __construct($usuario){
+    $this->usuario = $usuario;
+    $this->setSaludo();
 }
 
 private function setSaludo(){
-$this->msn = "Hola Mundo";
+$this->msn = "Bienvenid@ Sr/Sra";
 }
 
 public function getSaludo(){
 echo $this->msn;
 }
+
 }
